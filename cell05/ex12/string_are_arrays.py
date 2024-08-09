2148f81db3ded8990a1d32e334eec32d363619e7
Maria Adelia De Campos <madelia-@c3r2p5.42sp.org.br>
2024-08-09 14:31:57 -0300

    Adding a new way to do the logic

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -5,7 +5,6 @@ import re
 def string_z():
     const_z = "z"
     parameters = sys.argv[1:]
-   
     if len(parameters) == 1:        
         for p in parameters:
             count_zs = p.count(const_z)                       
@@ -18,6 +17,18 @@ def string_z():
     else:
         print("none")
 
+#List comprehension
+    # z = [i for i in parameters[0] if i == 'z']
+    # print(''.join(z))
+
+    #Explanation about how the superior code works
+    # z = []
+
+    # for parameter in parameters:
+    #     for i in parameter:
+    #         if i == 'z':
+    #             z.append(i)
+    # print(''.join(z))
 
 if __name__ == "__main__":
     string_z()
